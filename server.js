c297eb2658c5dd4f0e0992360bb8110d8358180c
Chris MacGeorge <christopher.s.macgeorge@gmail.com>
2020-10-29 19:27:19 -0700

    added html outline, routes, and frontedn js files

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const apiRoutes = require("./routes/apiRoutes")
+const htmlRoutes = require("./routes/htmlRoutes")
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/FitnessTracker", { useNewUrlParser: true });
+
+app.use(htmlRoutes)
+app.use(apiRoutes)
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
